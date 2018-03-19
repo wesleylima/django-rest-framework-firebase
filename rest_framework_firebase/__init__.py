ff0a4fea9f8e7be4f160025d72f51d31dfe1da7c
Wesley Lima <weslima@gmail.com>
2018-03-19 09:41:03 -0400

    New version for setup.py

--- rest_framework_firebase/__init__.py
+++ rest_framework_firebase/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'djangorestframework-firebase-auth'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Wesley Lima'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Fan Retreat, Inc.'

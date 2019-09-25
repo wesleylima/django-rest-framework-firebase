a8418684d2e68b0958ae497746e7664e8a36c263
Mandeep Gill <mandeep@nstack.com>
2019-09-25 13:32:07 +0100

    feat: Add anonymous auth support and fix race condition by making User creation atomic

--- rest_framework_firebase/authentication.py
+++ rest_framework_firebase/authentication.py
@@ -1,16 +1,18 @@
-import firebase_admin
-from firebase_admin import credentials
-from rest_framework_firebase.settings import api_settings
-from django.utils.encoding import smart_text
-from rest_framework import exceptions
-from firebase_admin import auth
-from django.utils.translation import ugettext as _
-from django.contrib.auth import get_user_model
-from django.db import IntegrityError
+import abc
 
+import firebase_admin
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.utils.encoding import smart_text
+from django.utils.translation import ugettext as _
+from firebase_admin import auth, credentials
+from rest_framework import exceptions
 from rest_framework.authentication import (
     BaseAuthentication, get_authorization_header
 )
+
+from rest_framework_firebase.settings import api_settings
+
 
 if api_settings.FIREBASE_ACCOUNT_KEY_FILE:
     creds = credentials.Certificate(api_settings.FIREBASE_ACCOUNT_KEY_FILE)
@@ -25,6 +27,10 @@ class BaseFirebaseAuthentication(BaseAuthentication):
     """
     Token based authentication using firebase.
     """
+
+    @abc.abstractmethod
+    def get_token(self, request):
+        ...
 
     def authenticate(self, request):
         """
@@ -44,23 +50,32 @@ class BaseFirebaseAuthentication(BaseAuthentication):
             msg = _('Could not log in.')
             raise exceptions.AuthenticationFailed(msg)
 
-        user = self.authenticate_credentials(payload)
+        user = self._authenticate_credentials(payload)
 
         return (user, payload)
 
-    def authenticate_credentials(self, payload):
+    def _authenticate_credentials(self, payload):
         """
         Returns an active user that matches the payload's user id and email.
         """
         User = get_user_model()
         uid_field = api_settings.FIREBASE_UID_FIELD
         uid = payload['uid']
+        anon_auth = False
+        user = None
         if not uid:
             msg = _('Invalid payload.')
             raise exceptions.AuthenticationFailed(msg)
         try:
-            if 'email_verified' not in payload or ('email_verified' in payload and not payload['email_verified']):
-                msg = _('User email not yet confirmed.')
+            if payload.get('email', None):
+                # a non-anon user
+                if 'email_verified' not in payload or ('email_verified' in payload and not payload['email_verified']):
+                    msg = _('User email not yet confirmed.')
+                    raise exceptions.AuthenticationFailed(msg)
+            elif payload["firebase"]["sign_in_provider"] == "anonymous":
+                anon_auth = True
+            else:
+                msg = _('Unknown auth method')
                 raise exceptions.AuthenticationFailed(msg)
             user = User.objects.get(**{uid_field: uid})
         except User.DoesNotExist:
@@ -68,31 +83,40 @@ class BaseFirebaseAuthentication(BaseAuthentication):
                 msg = _('Invalid signature.')
                 raise exceptions.AuthenticationFailed(msg)
 
-            # Make a new user here!
-            user = auth.get_user(uid)
-            # TODO: This assumes emails are unique. Factor this out as an option
+            # Try get user by email rather than uid
+            fb_user = auth.get_user(uid)
             try:
-                user = User.objects.get(email=user.email)
+                # NOTE: we only have the FB uid to id a django user in the anon case
+                if anon_auth:
+                    raise User.DoesNotExist()
+                if api_settings.FIREBASE_UNIQUE_EMAIL:
+                    user = User.objects.get(email=fb_user.email)
+                else:
+                    # get the lastest user using the email (assuming has the larger PK)
+                    user = User.objects.last(email=fb_user.email)
                 setattr(user, uid_field, uid)
                 user.save()
             except User.DoesNotExist:
-                fields = {
-                    uid_field: uid,
-                    'email': user.email
-                }
-                try:
-                    u = User(**fields)
-                    u.is_active = True
-                    u.save()
-                except IntegrityError:
-                    # The user has already been created from a concurrent request
-                    u = User.objects.get(**fields)
-                return u
+                # User absolutely doesn't exist - create atomically
+                user = self._create(User, uid, uid_field, fb_user)
 
         if not user.is_active:
             msg = _('User account is disabled.')
             raise exceptions.AuthenticationFailed(msg)
 
+        return user
+
+    @transaction.atomic
+    def _create(self, User, uid, uid_field, fb_user):
+        """Create user atomically"""
+        fields = {
+            uid_field: uid,
+            'email': fb_user.email or ''
+        }
+        (user, created) = User.objects.select_for_update().get_or_create(**fields)
+        if created:
+            user.is_active = True
+            user.save()
         return user
 
 
